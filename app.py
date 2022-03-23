0ce9fa37d61b06501b047a5987fddacdf038a94f
Mohammad Minul Islam <minul.islam@gmail.com>
2022-03-23 10:47:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -59,28 +59,28 @@ def display_value(continuous_var):
    # Create a grouped bar chart
    mydata1 = go.Bar(
       x=results.loc['Walt Disney Studios Motion Pictures'].index,
-      y=results.loc['Walt Disney Studios Motion Pictures'].values,
+      y=results.loc['Walt Disney Studios Motion Pictures'][continuous_var],
       name='Disney',
       marker=dict(color=color1)
       )
 
    mydata2 = go.Bar(
       x=results.loc['20th Century Fox'].index,
-      y=results.loc['20th Century Fox'].values,
+      y=results.loc['20th Century Fox'][continuous_var],
       name='Fox',
       marker=dict(color=color2)
       )
 
    mydata3 = go.Bar(
       x=results.loc['Sony Pictures'].index,
-      y=results.loc['Sony Pictures'].values,
+      y=results.loc['Sony Pictures'][continuous_var],
       name='Sony Pictures',
       marker=dict(color=color3)
       )
 
    mydata4 = go.Bar(
       x=results.loc['Other'].index,
-      y=results.loc['Other'].values,
+      y=results.loc['Other'][continuous_var],
       name='Other',
       marker=dict(color=color4)
       )
